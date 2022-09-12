9f735b1295cb91bf0592624f7ae245205a1a6095
dbcalitis <daria.bernice.calitis@mths.ca>
2022-09-12 22:08:09 +0000

    init

---
+++ HelloWorld.java
@@ -0,0 +1,39 @@
+/*
+* The HelloWorld program implements an application that
+ * simply displays "Hello World!" to the standard output.
+*
+* @author  Daria Bernice Calitis
+* @version 1.0
+* @since   2022-09-12
+*/
+
+/**
+* This is the standard "Hello, World!" program.
+*/
+final class HelloWorld {
+
+  /**
+  * Prevent instantiation.
+  * Throw an exception IllegalStateException.
+  * if this ever is called
+  *
+  * @throws IllegalStateException
+  *
+  */
+  private HelloWorld() {
+    throw new IllegalStateException("Cannot be instantiated");
+  }
+
+  /**
+  * The starting main() function.
+  *
+  * @param args No args will be used
+  */
+  public static void main(final String[] args) {
+
+    // print out "Hello, World!"
+    System.out.println("Hello, World!");
+
+    System.out.println("\nDone.");
+  }
+}
---
+++ package-info.java
@@ -0,0 +1,9 @@
+/**
+* This is the package-info.java file.
+*
+* @author  Daria Bernice Calitis
+* @version 1.0
+* @since   2022-09-12
+*/
+package ca.mths.ics4u;
+
